ad825a6ff67ee698d66a391ea5fb2ccb898684cf
yanpatla <tomipatlayan@gmail.com>
2026-01-21 17:41:20 +0200

    set up cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/project.route";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 connectDB();
 const app = express();
+app.use(cors(corsConfig));
 app.use(express.json());
 // Routes
 app.use("/api/projects", projectRoutes);
